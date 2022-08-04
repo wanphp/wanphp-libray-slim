1dea73a4e4e6b539b9d45aeae0f2f4507599e43f
火子 <yxyong@126.com>
2022-08-04 09:11:21 +0800

    环境配置

---
+++ Setting.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Wanphp\Libray\Slim;
+
+class Setting
+{
+  private array $settings;
+
+  public function __construct(array $settings)
+  {
+    $this->settings = $settings;
+  }
+
+  /**
+   * @param string $key
+   * @param $value
+   * @return void
+   */
+  public function set(string $key, $value)
+  {
+    $this->settings[$key] = $value;
+  }
+
+  /**
+   * @param string $key
+   * @return mixed
+   */
+  public function get(string $key = ''): mixed
+  {
+    return empty($key) ? $this->settings : $this->settings[$key];
+  }
+}

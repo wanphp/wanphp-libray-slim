82064af5e9969bce5ac7d3a36fcb50570826c745
火子 <yxyong@126.com>
2023-04-03 10:12:40 +0800

    设置允许上传文件

--- UploaderInterface.php
+++ UploaderInterface.php
@@ -24,8 +24,20 @@ interface UploaderInterface
 
   /**
    * 删除文件
-   * @param int $id
+   * @param string $file 文件ID或文件路径
    * @return int
    */
-  public function delFile(int $id): int;
+  public function delFile(string $file): int;
+
+  /**
+   * 允许上传文件扩展名
+   * @param array $extension
+   */
+  public function allowExtension(array $extension): void;
+
+  /**
+   * 允许上传文件类型
+   * @param array $fileType
+   */
+  public function allowFileType(array $fileType): void;
 }
